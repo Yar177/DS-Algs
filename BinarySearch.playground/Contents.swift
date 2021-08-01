7edc623697b18f9a954762f53b1326cf0de69c9b
Yar177 <yarsher.dev@gmail.com>
2021-08-01 11:43:27 -0400

    binarySearch

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
 var greeting = "Hello, playground"
+var searchArray:[Int] = []
+for i in 0...2100{
+    searchArray.append(i)
+}
+//print(searchArray)
+
+func binarySearch(item:Int, array:[Int]) -> Int{
+    var low = 0
+    var high:Int = array.count - 1
+    var conter = 0
+    while low <= high{
+        print("count #\(conter)")
+        conter += 1
+        var mid = (low + high) / 2
+        if array[mid] == item{
+            return mid
+        }else if mid > item{
+            high = mid - 1
+        }else if mid < item{
+            low = mid + 1
+        }
+    }
+    return 0
+}
+
+binarySearch(item: 2233, array: searchArray)
+
+
+
+
+
+
