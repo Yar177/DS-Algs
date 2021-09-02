b6895182149c1dc80d9044bd837bc284b7b00e60
Yar177 <yarsher.dev@gmail.com>
2021-09-02 01:29:53 -0400

    towersOfHanoi

--- BinarySearch.playground/Pages/StackTowersOfHanoi.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/StackTowersOfHanoi.xcplaygroundpage/Contents.swift
@@ -2,8 +2,6 @@
 
 import Foundation
 
-
-
 public class Stack<T>: CustomStringConvertible{
     private var container: [T] = [T]()
     public func push(_ thing: T){container.append(thing)}
@@ -11,6 +9,29 @@ public class Stack<T>: CustomStringConvertible{
     public var description: String { return container.description}
 }
 
+var numDiscs = 3
+var towerA = Stack<Int>()
+var towerB = Stack<Int>()
+var towerC = Stack<Int>()
+for i in 1..<numDiscs{
+    towerA.push(i)
+}
+
+func hanoi(from: Stack<Int>, to: Stack<Int>, temp: Stack<Int>, n: Int){
+    if n == 1 {
+        to.push(from.pop())
+    }else{
+        hanoi(from: from, to: temp, temp: to, n: n - 1)
+        hanoi(from: from, to: to, temp: temp, n: 1)
+        hanoi(from: temp, to: to, temp: from, n: n - 1)
+    }
+}
+
+hanoi(from: towerA, to: towerC, temp: towerB, n: numDiscs)
+
+print(towerA)
+print(towerB)
+print(towerC)
 
 
 
