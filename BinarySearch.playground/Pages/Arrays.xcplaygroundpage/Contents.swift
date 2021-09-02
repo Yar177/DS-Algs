6a36587bb604a1e844d015c6f8b51d09603a9ab8
Yar177 <yarsher.dev@gmail.com>
2021-09-02 16:07:23 -0400

    arrayQ3

--- BinarySearch.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -64,8 +64,12 @@ func rotateRightOnce(A: [Int]) -> [Int]{
 
 solution([2,3,6,7,1], 3)
 
-//==================================================================================
 
+
+
+
+//=========================================
+//My soulution
 
 
 func formatString(_ s: String) -> String{
@@ -80,18 +84,17 @@ func formatString(_ s: String) -> String{
 
 func cleanString(_ startSting: String) -> String{
     let acceptedChars = Set("0123456789")
-    return startSting.filter { acceptedChars.contains($0)
-    }
+    return startSting.filter { acceptedChars.contains($0)}
 }
 
 formatString("9dids09vds0 0 s0d0=ljkdsa8848jdfs3id8- ;aedk30scv21=fvds p")
 formatString("0099")
 
 
-//==================================================================================
+//========
+//Their solution
 
-
-func solution(_ S: String) -> String{
+func solutionQ2(_ S: String) -> String{
     let noSpace = S.replacingOccurrences(of: " ", with: "")
     let noDashesNoSpace = noSpace.replacingOccurrences(of: "-", with: "")
     
@@ -107,10 +110,44 @@ func solution(_ S: String) -> String{
     return res
 }
 
-solution("909 03309 09323-3-33 0033---   99")
-solution("009")
+solutionQ2("909 03309 09323-3-33 0033---   99")
+solutionQ2("009")
 
 
+//=================================================================================================
+//My soulution
+
+
+let A = ["pim" , "pom"]
+let B = ["9999999999", "777888999"]
+let C = "88999"
+
+func findContact(_ contactList:[String], _ phoneNums:[String], _ searchTerm: String) -> String{
+    let contact = "No Contact Found"
+    for i in 0...phoneNums.count{
+        if phoneNums[i].contains(searchTerm) {
+            return contactList[i]
+        }
+    }
+    return contact
+}
+
+func findContactFilter(_ contactList:[String], _ phoneNums:[String], _ searchTerm: String) -> String{
+    let filteredArray = phoneNums.filter{$0.contains(searchTerm)}
+    for element in filteredArray{
+        return contactList[phoneNums.firstIndex(of: element)!]
+    }
+    let contact = filteredArray.isEmpty ? "No Contact Found" : filteredArray.first
+    return  contact!
+}
+
+findContactFilter(A, B, C)
+findContact(A, B, C)
+
+
+
+//==============
+//Their soulution
 
 
 
