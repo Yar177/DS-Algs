122909075f5b95452f8b298cc5f70d09a0cffb61
Yar177 <yarsher.dev@gmail.com>
2021-09-03 17:35:50 -0400

    linearSearch

--- BinarySearch.playground/Pages/Search.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Search.xcplaygroundpage/Contents.swift
@@ -2,6 +2,123 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var greeting = "Hello, Search"
+print(greeting)
+
+enum Nucleatide: Character, Comparable{
+    static func < (lhs: Nucleatide, rhs: Nucleatide) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+    
+    case A = "A", C = "C", G = "G", T = "T"
+}
+
+typealias Codon = (Nucleatide, Nucleatide, Nucleatide)
+typealias Gene = [Codon]
+
+let geneSequence = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+func stringToGene(_ s: String) -> Gene{
+    var gene = Gene()
+    for i in stride(from: 0, through: s.count, by: 3){
+        guard (i + 2) < s.count else {return gene}
+        if let n1 = Nucleatide.init(rawValue: s[s.index(s.startIndex, offsetBy: i)]),
+           let n2 = Nucleatide.init(rawValue: s[s.index(s.startIndex, offsetBy: i + 1)]),
+           let n3 = Nucleatide.init(rawValue: s[s.index(s.startIndex, offsetBy: i + 2)]){
+            gene.append((n1, n2, n3))
+        }
+    }
+    return gene
+}
+
+var gene = stringToGene(geneSequence)
+print("\(gene)")
+
+
+
+//MARK: 2.1.2. Linear search
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
