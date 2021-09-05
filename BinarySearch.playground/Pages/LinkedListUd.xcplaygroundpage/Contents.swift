965d4d6838345f040bdc6c44cd98d71d08921d16
Yar177 <yarsher.dev@gmail.com>
2021-09-05 09:56:48 -0400

    LL add

--- BinarySearch.playground/Pages/LinkedListUd.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/LinkedListUd.xcplaygroundpage/Contents.swift
@@ -10,21 +10,134 @@ print(greeting)
 
 //MARK: LinkedList
 
-class Node<T>{
-    var data: T
+class Node{
+    var data: Int
     var next: Node?
     
-    init(_ data: T, _ next: Node?) {
+    init(_ data: Int, _ next: Node? = nil) {
         self.data = data
         self.next = next
     }
 }
 
-let node1 = Node(1, node2)
+let node3 = Node(3)
 let node2 = Node(2, node3)
-let node3 = Node(3, nil)
 
-print(node2.data)
+let node1 = Node(1, node2)
+
+//print(node2.data)
+
+func printLinkedList(_ headNode: Node){
+    print(headNode.data)
+    var node = headNode
+    while node.next != nil {
+        print(node.next!.data)
+        node = node.next!
+    }
+}
+
+func printLinkedList1(_ head: Node?){
+    if head == nil {return}
+    
+    var node = head
+    print(node!.data)
+    
+    while node?.next != nil {
+        print(node!.next!.data)
+        node = node?.next
+    }
+}
+
+printLinkedList(node1)
+
+print(node1.next!.data)
+
+
+
+class LinkList {
+    private var head: Node?
+        
+    func addFront(_ data: Int) {
+        let newNode = Node(data)
+        newNode.next = head
+        head = newNode
+    }
+
+    func getFirst() -> Int? {
+        guard head != nil else {
+            return nil
+        }
+        return head!.data
+    }
+
+    func addBack(_ data: Int) {
+        let newNode = Node(data)
+        
+        if head == nil {
+            head = newNode
+            return
+        }
+        
+        var node = head!
+        while(node.next != nil){
+            
+        }
+    }
+
+    func getLast() -> Int? {
+        return nil
+    }
+
+    func insert(position: Int, data: Int) {
+    }
+    
+    func deleteFirst() {
+    }
+
+    func deleteLast() {
+    }
+    
+    func delete(at position: Int) {
+    }
+    
+    var isEmpty: Bool {
+        return false
+    }
+    
+    func clear() {
+    }
+    
+    func printLinkedList() {
+        if head == nil { return }
+        
+        var result = [Int]()
+        var node = head
+        result.append(node!.data)
+        
+        while node?.next != nil {
+            result.append(node!.next!.data)
+            node = node?.next
+        }
+        
+        print(result)
+    }
+}
+
+let linkedList = LinkList()
+linkedList.addFront(3)
+linkedList.addFront(2)
+linkedList.addFront(1)
+
+linkedList.printLinkedList()
+
+linkedList.addFront(4)
+
+linkedList.printLinkedList()
+
+
+
+
+
 
 
 
