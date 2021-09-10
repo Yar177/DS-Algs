270a9f9331633ec0cdf95ff593fee7394c9b19af
Yar177 <yarsher.dev@gmail.com>
2021-09-09 23:40:26 -0400

    stack

--- BinarySearch.playground/Pages/Stack.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -2,7 +2,21 @@
 
 import Foundation
 
+struct Stack<Element>{
+    private var storage: [Element] = []
+    init() {}
+    
+}
 
+
+extension Stack: CustomStringConvertible{
+    var description: String{
+        let topDivider = "--------Top--------"
+        let bottomDivider = "--------End--------"
+        let stackElements = storage.map{"\($0)"}.reversed().joined(separator: "\n")
+        return topDivider + stackElements + bottomDivider
+    }
+}
 
 
 
