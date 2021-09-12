b4ec32bf9a46e87caa8b959bb7a9529c585489af
Yar177 <yarsher.dev@gmail.com>
2021-09-11 23:18:08 -0400

    Queue

--- BinarySearch.playground/Pages/Stack.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -6,18 +6,38 @@ struct Stack<Element>{
     private var storage: [Element] = []
     init() {}
     
+    mutating func push(_ element: Element){
+        storage.append(element)
+    }
+    
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+    
 }
 
 
 extension Stack: CustomStringConvertible{
     var description: String{
-        let topDivider = "--------Top--------"
-        let bottomDivider = "--------End--------"
+        let topDivider = "----Top----\n"
+        let bottomDivider = "\n----End----"
         let stackElements = storage.map{"\($0)"}.reversed().joined(separator: "\n")
         return topDivider + stackElements + bottomDivider
     }
 }
 
+var stack = Stack<Int>()
+
+stack.push(20)
+stack.push(1)
+stack.push(12)
+stack.push(230)
+stack.push(4)
+stack.push(3)
+print(stack)
+
+stack.pop()
+print(stack)
 
 
 
