f4ee3a294427a0ecc27cd963285ea7357c1559d1
Yar177 <yarsher.dev@gmail.com>
2021-09-12 09:02:01 -0400

    binaryTree

--- BinarySearch.playground/Pages/Trees.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Trees.xcplaygroundpage/Contents.swift
@@ -2,6 +2,29 @@
 
 import Foundation
 
+struct Queue<Element>{
+    var elements:[Element] = []
+    mutating func enqueue(_ value: Element) -> Bool{
+         elements.append(value)
+        return true
+    }
+    
+    var isEmpty:Bool{
+        return elements.isEmpty
+    }
+    mutating func dequeue() -> Element?{
+        return isEmpty ? nil : elements.removeFirst()
+    }
+    
+}
+
+var q = Queue<Int>()
+q.enqueue(3)
+q.enqueue(33)
+q.enqueue(2)
+print(q)
+
+
 class TreeNode<T>{
     var value: T
     var children: [TreeNode] = []
@@ -21,7 +44,7 @@ let beverages = TreeNode<String>("Beverages")
 let hot = TreeNode<String>("Hot")
 let cold = TreeNode<String>("Cold")
 
-let tee = TreeNode<String>("Tea")
+let tea = TreeNode<String>("Tea")
 let coffee = TreeNode<String>("Coffee")
 
 hot.add(tea)
@@ -36,7 +59,7 @@ cold.add(milk)
 beverages.add(hot)
 beverages.add(cold)
 
-print(beverages.children[0].value)
+//print(beverages.children[0].value)
 
 //Depth First Traversal
 extension TreeNode{
@@ -51,6 +74,82 @@ extension TreeNode{
 beverages.depthFirst{
     print($0.value)
 }
+
+beverages.lelveOrder{
+    print($0.value)
+}
+
+//LevelOrder Traversal
+extension TreeNode{
+    func lelveOrder(_ visit: (TreeNode) -> Void) {
+        visit(self)
+        var queue = Queue<TreeNode>()
+        children.forEach{
+            queue.enqueue($0)
+        }
+        while let node = queue.dequeue() {
+            visit(node)
+            node.children.forEach{
+                queue.enqueue($0)
+            }
+        }
+    }
+}
+
+extension TreeNode where T: Equatable{
+    func search(_ value: T) -> TreeNode?{
+        var result: TreeNode?
+        lelveOrder{ node in
+            if node.value == value{
+            result = node
+            }
+        }
+        return result
+    }
+}
+
+if let sRes = beverages.search("Soda"){
+    print(sRes.value)
+}
+
+
+
+class BinaryTreeNode<Element>{
+    var value: Element
+    var leftChild: BinaryTreeNode?
+    var righChild: BinaryTreeNode?
+    init(_ value: Element) {
+        self.value = value
+    }
+}
+
+
+let ten = BinaryTreeNode(10)
+let nine = BinaryTreeNode(9)
+let two = BinaryTreeNode(2)
+let one = BinaryTreeNode(1)
+let three = BinaryTreeNode(3)
+let four = BinaryTreeNode(4)
+let six = BinaryTreeNode(6)
+let five = BinaryTreeNode(5)
+
+ten.leftChild = nine
+ten.righChild = two
+nine.leftChild = one
+nine.righChild = three
+two.leftChild = four
+two.righChild  = six
+
+
+
+
+
+
+
+
+
+
+
 
 
 
