50ed81fb348c0909c719ee385ef99b430f8fe552
Yar177 <yarsher.dev@gmail.com>
2021-09-11 23:41:23 -0400

    q

--- BinarySearch.playground/Pages/Queues.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/Queues.xcplaygroundpage/Contents.swift
@@ -2,5 +2,53 @@
 
 import Foundation
 
+struct Queue<T>{
+    
+    var array:[T] = []
+    
+    init() {}
+    
+    var isEmpty:Bool {
+        return array.isEmpty
+    }
+    
+    var peek: T? {
+        return array.first
+    }
+    
+    mutating func enqueue(_ element: T) -> Bool{
+        array.append(element)
+        return true
+    }
+    
+    mutating func dequeue() -> T?{
+        return isEmpty ? nil : array.removeFirst()
+    }
+    
+}
+
+extension Queue: CustomStringConvertible{
+    var description:String{
+        return String(describing: array)
+    }
+}
+
+var queue = Queue<Int>()
+queue.enqueue(10)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(7)
+print(queue)
+
+let item = queue.dequeue()
+print(item)
+print(queue)
+
+
+
+
+
+
+
 
 //: [Next](@next)
