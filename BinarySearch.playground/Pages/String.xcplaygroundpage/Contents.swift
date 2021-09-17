84e323507e995c4154dd9819e9c7d3b1b969ec48
Yar177 <yarsher.dev@gmail.com>
2021-09-16 23:59:46 -0400

    s

--- BinarySearch.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -4,8 +4,30 @@ import Foundation
 
 
 var str = "abssscded"
-str.count()
-for i in 0...5{
-    print(str[i])
+str.count
+
+for (n, c) in "Swift".enumerated() {
+    print("\(n): '\(c)'")
 }
+
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var maxLength = 0
+       for (i, c) in s.enumerated() {
+            print("\(i): '\(c)'")
+           var count = 0
+           for cr in (i+1)...s.count{
+               print(count += 1)
+
+           }
+        }
+        
+        return maxLength
+    }
+}
+    
+//for i in 0...5{
+//   // print(str[i])
+//}
 //: [Next](@next)
