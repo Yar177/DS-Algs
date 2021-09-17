2eb4e16240626220e526c67550e652323e310102
Yar177 <yarsher.dev@gmail.com>
2021-09-17 15:31:37 -0400

    FizzBuzz

--- BinarySearch.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -26,6 +26,24 @@ class Solution {
         return maxLength
     }
 }
+
+let index = String.Index(encodedOffset: 0)
+print(str[index])
+
+
+
+for i in 1...100{
+    if i % 15 == 0 {
+        print("FizzBuzz")
+    }else if i % 3 == 0{
+        print("Fizz")
+    }else if i % 5 == 0{
+        print("Buzz")
+    }else {
+        print(1)
+    }
+}
+
     
 //for i in 0...5{
 //   // print(str[i])
