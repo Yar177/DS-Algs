e1fd2b1eba2b415608187907814812a67fbc113a
Yar177 <yarsher.dev@gmail.com>
2021-09-21 23:29:44 -0400

    graphs

--- BinarySearch.playground/Pages/SortingAlgs.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/SortingAlgs.xcplaygroundpage/Contents.swift
@@ -2,6 +2,32 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+class BubbleSort{
+    func sort(_ array:[Int]) -> [Int]{
+        var arr = array
+        let n = arr.count
+        for i in 0..<n-1{
+            for j in 0..<n-i-1{
+                if arr[j] > arr[j+1]{
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j + 1] = temp
+                }
+            }
+        }
+        return arr
+    }
+}
+
+let a = [2,4,5,2,1,4,5,6,3]
+let bub = BubbleSort()
+bub.sort(a)
+
+
+func mergeSoer(_ array:[Int]) -> [Int]{
+    
+}
+
 
 //: [Next](@next)
