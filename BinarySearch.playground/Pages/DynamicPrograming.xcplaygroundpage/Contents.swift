193ba7feff3ed8ba38c6c9035448f54ee1707c6f
Yar177 <yarsher.dev@gmail.com>
2021-09-21 22:48:28 -0400

    emo

--- BinarySearch.playground/Pages/DynamicPrograming.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/DynamicPrograming.xcplaygroundpage/Contents.swift
@@ -2,6 +2,26 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+var memo = [Int : Int]()
+
+func fib(_ n: Int) -> Int{
+    
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    }
+    
+    if let results = memo[n] {return results}
+    
+    memo[n] = fib(n - 1) + fib(n - 2)
+    
+    return memo[n]
+    
+}
+
+
+
 
 //: [Next](@next)
