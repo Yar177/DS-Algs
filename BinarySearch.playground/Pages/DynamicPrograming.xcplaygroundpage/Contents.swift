9d5fce990b9346fc8e349aaa34b8e2708ababf0c
Yar177 <yarsher.dev@gmail.com>
2021-09-21 22:58:15 -0400

    sort

--- BinarySearch.playground/Pages/DynamicPrograming.xcplaygroundpage/Contents.swift
+++ BinarySearch.playground/Pages/DynamicPrograming.xcplaygroundpage/Contents.swift
@@ -4,24 +4,28 @@ import Foundation
 
 
 var memo = [Int : Int]()
-
 func fib(_ n: Int) -> Int{
-    
-    if n == 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    }
-    
+    if n == 0 {return 0}
+    else if n == 1 {return 1}
     if let results = memo[n] {return results}
-    
     memo[n] = fib(n - 1) + fib(n - 2)
-    
-    return memo[n]
-    
+    return memo[n]!
+}
+
+
+func fibNaive(_ n: Int) -> Int{
+    if n == 0 {return 0}
+    else if n == 1 {return 1}
+    else {
+        return fibNaive(n - 1) + fibNaive(n - 2)
+    }
 }
 
 
 
+fibNaive(44)
+fib(20)
+
+print(memo)
 
 //: [Next](@next)
