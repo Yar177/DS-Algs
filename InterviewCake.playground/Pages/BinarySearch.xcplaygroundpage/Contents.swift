d0d73be7df9ef0aa2cb3672fa9a8564b94e31edf
Yar177 <yarsher.dev@gmail.com>
2021-10-05 22:05:08 -0400

    binarySearch

--- InterviewCake.playground/Pages/BinarySearch.xcplaygroundpage/Contents.swift
+++ InterviewCake.playground/Pages/BinarySearch.xcplaygroundpage/Contents.swift
@@ -3,16 +3,31 @@
 import Foundation
 
 func binarySearch(for input:Int, in arr:[Int]) -> Bool{
+    //floorIndex and ceillingIndex as walls
+    var floorIndex = 0
+    var ceillingIndex = arr.count
     
-    
-    
-    
+    while floorIndex < ceillingIndex{
+        let distance = ceillingIndex - floorIndex
+        let halfDistance = distance / 2
+        let guessIndex = floorIndex + halfDistance
+        
+        let guessVal = arr[guessVal]
+        if guessVal == input{
+            return true
+        }
+        if guessVal > input{
+            ceillingIndex = guessIndex
+        }else{
+            floorIndex = guessIndex
+        }
+    }
     return false
 }
 
 
 
-
+binarySearch(for 3, in [3,4,5,6,3,2,1])
 
 
 
