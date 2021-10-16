b37fb2322e21f72881691c0ee9dfdd884f1d9910
Yar177 <yarsher.dev@gmail.com>
2021-10-16 00:32:32 -0400

    int reverse

--- Sorting.playground/Pages/ArrayAndStringManipulationIC.xcplaygroundpage/Contents.swift
+++ Sorting.playground/Pages/ArrayAndStringManipulationIC.xcplaygroundpage/Contents.swift
@@ -44,14 +44,64 @@ dynamicArray.append(33)
 print(dynamicArray)
 print(fixedArray)
 
+class Meeting: CustomStringConvertible{
+    var startTime: Int
+    var endTime: Int
+    
+    init(startTime: Int, endTime: Int){
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+    var description: String{
+        return "(\(startTime), \(endTime))"
+    }
+}
 
 
+let meeting1 = Meeting(startTime: 2, endTime: 3)
+let meeting2 = Meeting(startTime: 6, endTime: 9)
+
+var allMeetings = [
+    Meeting(startTime: 0, endTime: 1),
+    Meeting(startTime: 3, endTime: 5),
+    Meeting(startTime: 4, endTime: 8),
+    Meeting(startTime: 10, endTime: 12),
+    Meeting(startTime: 9, endTime: 10)
+]
+func mergeRange(_ meetings:[Meeting]) -> [Meeting]{
+    var sortedMeetings = meetings.sorted(by: {$0.startTime < $1.startTime})
+    var mergeMeetings = [sortedMeetings[0]]
+    for i in 1..<sortedMeetings.count{
+        let currentMeeting = sortedMeetings[i]
+        let lastMegedMeeting = mergeMeetings[mergeMeetings.count - 1]
+        if currentMeeting.startTime <= lastMegedMeeting.endTime{
+            lastMegedMeeting.endTime = max(lastMegedMeeting.endTime, currentMeeting.endTime)
+        }else{
+            mergeMeetings.append(currentMeeting)
+        }
+    }
+    return sortedMeetings
+}
+
+print(mergeRange(allMeetings))
 
 
+func isPalindreeome(_ x: Int) -> Bool{
+    var num = x
+    var reveresed = 0
+    while num > 0{
+        let reminder = num % 10
+        print(reminder)
+        reveresed = reveresed * 10 + reminder
+        print(reveresed)
+        num = num / 10
+        print(num)
+    }
+    return x == reveresed
+}
 
 
-
-
+isPalindreeome(181)
 
 
 
