3cfa85d4e8819782382da955d05a898316bfaeea
Yar177 <yarsher.dev@gmail.com>
2021-10-24 10:20:36 -0400

    cls

--- InterviewCake.playground/Pages/ArraysMethods.xcplaygroundpage/Contents.swift
+++ InterviewCake.playground/Pages/ArraysMethods.xcplaygroundpage/Contents.swift
@@ -2,6 +2,47 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var phrase = "The rain in Spain stays mainly in the plain."
+
+let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
+phrase.removeAll(where: { vowels.contains($0) })
+
+print(phrase)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
--- InterviewCake.playground/Pages/Graphs.xcplaygroundpage/Contents.swift
+++ InterviewCake.playground/Pages/Graphs.xcplaygroundpage/Contents.swift
@@ -2,6 +2,163 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+//biderectional edges can travel in both directions vs diagraph
+public protocol Edge: CustomStringConvertible{
+    var u: Int {get set} //index of the from vertex
+    var v: Int {get set} //index of the to vertex
+    
+    var reversed: Edge {get}
+}
+
+
+protocol Graph: class, CustomStringConvertible{
+    associatedtype VertexType: Equatable
+    associatedtype EdgeType: Edge
+    
+    var verticies: [VertexType] { get set}
+    var edges: [[EdgeType]] {get set}
+}
+
+extension Graph{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//vertex matrix of adjency list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
