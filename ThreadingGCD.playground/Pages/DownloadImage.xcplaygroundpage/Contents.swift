8dc9196b73025c5b626da99c36a2e76577d3cd89
Yar177 <yarsher.dev@gmail.com>
2021-11-10 09:33:22 -0500

    delegates

--- ThreadingGCD.playground/Pages/DownloadImage.xcplaygroundpage/Contents.swift
+++ ThreadingGCD.playground/Pages/DownloadImage.xcplaygroundpage/Contents.swift
@@ -2,9 +2,11 @@
 
 import Foundation
 import UIKit
+import Combine
 
 let stringUrl = "https://www.solarsystemscope.com/textures/download/2k_earth_daymap.jpg"
 let url = URL(string: "https://www.solarsystemscope.com/textures/download/2k_earth_daymap.jpg")
+
 DispatchQueue.global().async {
     guard let data = try? Data(contentsOf: url!) else{return}
     DispatchQueue.main.async {
@@ -30,6 +32,7 @@ concurrentQueue.async {
 //    UINavigationController.pushViewController(vc, animated: true)
 //}
 
+//MARK: download img
 DispatchQueue.global(qos: .userInteractive).async {
     guard let data = try? Data(contentsOf: url!) else{return}
     DispatchQueue.main.async {
@@ -44,6 +47,61 @@ inactiveQueue.async {
 }
 inactiveQueue.activate()
 
+
+//MARK: execute after delay
+let myQueue = DispatchQueue(label: "my_queue")
+let timeDelay = DispatchTime.now() + 3
+myQueue.asyncAfter(deadline: timeDelay, qos: .userInitiated) {
+    print("3 sec delay")
+}
+
+
+
+//MARK: Deadlock
+//let mySerialQueue = DispatchQueue(label: "my_s_queue")
+//mySerialQueue.async {
+//    mySerialQueue.sync {
+//        print("inner block")
+//    }
+//    print("outer block")
+//}
+class RecurrsiveLock: NSObject{
+    var recursiveLock = NSRecursiveLock()
+    func safeMethod(){
+        recursiveLock.lock()
+        someFunc()
+        recursiveLock.unlock()
+    }
+    
+    func someFunc(){
+        recursiveLock.lock()
+        print("\(#function)")
+        recursiveLock.unlock()
+    }
+}
+
+var recLock = RecurrsiveLock()
+recLock.safeMethod()
+recLock.someFunc()
+
+
+
+
+let newSubject = PassthroughSubject<Int, Never>()
+let test = newSubject.sink { (value) in
+    print(Thread.isMainThread)
+}
+newSubject.send(1)
+
+DispatchQueue.global().async {
+    newSubject.send(4)
+}
+
+
+
+
+
+//MARK: cache image
 let imageCache = NSCache<NSString, UIImage>()
 extension UIImageView{
     func loadImageCachedImg(_ url: String){
@@ -52,25 +110,21 @@ extension UIImageView{
             self.image = cachedImage
             return
         }
-        
         if let url = URL(string: url){
             URLSession.shared.dataTask(with: url, completionHandler: {(data, response, errror) in
                 if errror != nil{
                     print("\(String(describing:errror))")
                     return
                 }
-                
                 DispatchQueue.main.async {
                     if let data = data{
                         if let downloadedImage = UIImage(data: data){
                             // imageCache.setObject(downloadedImage, forKey: NSString(string: url))
                             imageCache.setObject(downloadedImage, forKey: NSString(string: "url"))
                             self.image = downloadedImage
-                            
                         }
                     }
                 }
-                
             }).resume()
         }
     }
