31f8d958928ffcbe41661078029be16cafb61803
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-06 10:00:16 -0500

    cls

--- SwiftGeneral.playground/Pages/SizeClasses.xcplaygroundpage/Contents.swift
+++ SwiftGeneral.playground/Pages/SizeClasses.xcplaygroundpage/Contents.swift
@@ -5,9 +5,6 @@ import Foundation
 
 
 
-let widithSizeClass = traitCollection.horizontalSizeClass
-let heightSizeClass = traitCollection.verticalSizeClass
-print("w: \(widithSizeClass) - h:\(heightSizeClass)")
 //print("w: \(sizeClassString(sizeClass: widithSizeClass)) - h:\(sizeClassString(sizeClass: heightSizeClass))")
 
 
@@ -22,10 +19,46 @@ func screenSize(sizeClass: UIUserInterfaceSizeClass) -> String{
     }
 }
 
+print("print")
 
+class ViewControllerTest: UIViewController{
+    
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let widithSizeClass = traitCollection.horizontalSizeClass
+        let heightSizeClass = traitCollection.verticalSizeClass
+        print("w: \(widithSizeClass) - h:\(heightSizeClass)")
+//        self.printSize("w: \(widithSizeClass) - h:\(heightSizeClass)")
+        self.screenSize(sizeClass: widithSizeClass)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.horizontalSizeClass == .compact {
+            // load slim view
+        } else {
+            // load wide view
+        }
+    }
+    
+    func printSize(_ printString: String){
+        print("\(printString)")
+    }
+   
+    func screenSize(sizeClass: UIUserInterfaceSizeClass) -> String{
+        switch sizeClass{
+        case .unspecified:
+            return "unspecified"
+        case .compact:
+            return "compact"
+        case .regular:
+            return "regular"
+        }
+    }
+}
 
-
+let vc = ViewControllerTest()
+vc.viewWillLayoutSubviews()
 
 
 
