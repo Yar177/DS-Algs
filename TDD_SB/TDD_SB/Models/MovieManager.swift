868588cdfba1add38a751b41bec9db80477211bf
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-07 17:24:48 -0500

    MovieManager

---
+++ TDD_SB/TDD_SB/Models/MovieManager.swift
@@ -0,0 +1,14 @@
+//
+//  MovieManager.swift
+//  TDD_SB
+//
+//  Created by Hoshiar Sher on 12/7/21.
+//
+
+import Foundation
+
+
+class MovieManager{
+    
+    
+}
---
+++ TDD_SB/TDD_SBTests/TestModels/MovieManagerTests.swift
@@ -0,0 +1,28 @@
+//
+//  MovieManagerTests.swift
+//  TDD_SBTests
+//
+//  Created by Hoshiar Sher on 12/7/21.
+//
+
+import XCTest
+@testable import TDD_SB
+
+class MovieManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+ 
+    func testInit_MoviesToSee_ReturnZero(){
+        let sut = MovieManager()
+        
+        
+    }
+
+}
