13c4ea1156ba204c8058a87eeaa4a979b6afd8d2
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-07 16:08:48 -0500

    TDD init

---
+++ TDD/Shared/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  Shared
+//
+//  Created by Hoshiar Sher on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDD_SB/TDD_SB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDD_SB
+//
+//  Created by Hoshiar Sher on 12/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
