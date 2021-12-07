2c88fdb1433225fff79ea70ec3a7b21391dc79a1
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-07 18:00:57 -0500

    movieAtIndex

--- TDD_SB/TDD_SB/Models/MovieManager.swift
+++ TDD_SB/TDD_SB/Models/MovieManager.swift
@@ -9,6 +9,14 @@ import Foundation
 
 
 class MovieManager{
+    var moviesToSee = 0
+    var movieSeenCount = 0
+    private var moviesToSeeArray = [Movie]()
+    func addMovie(movie: Movie){
+        moviesToSee += 1
+    }
     
-    
+    func movieAtIndex(index: Int) -> Movie{
+        return Movie(title: "Test Movie")
+    }
 }
--- TDD_SB/TDD_SBTests/TestModels/MovieManagerTests.swift
+++ TDD_SB/TDD_SBTests/TestModels/MovieManagerTests.swift
@@ -9,6 +9,8 @@ import XCTest
 @testable import TDD_SB
 
 class MovieManagerTests: XCTestCase {
+    
+    var sut:MovieManager!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,12 +19,43 @@ class MovieManagerTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
- 
+    
+    override func setUp() {
+        super.setUp()
+        sut = MovieManager()
+    }
+ //MARK: INIT MOVIEMANAGER
     func testInit_MoviesToSee_ReturnZero(){
-        let sut = MovieManager()
-        
-        
+        let moviesToSee = 0
+        XCTAssertEqual(sut.moviesToSee, moviesToSee)
+    }
+    
+    func testInit_MoviesScene_ReturnsZero(){
+        let movieSeenCount = 0
+        XCTAssertEqual(sut.movieSeenCount, movieSeenCount)
     }
 
+//    MARK: Add & Query
+    
+    func tetsAdd_MoviesToSee_ReturnOne(){
+        let title = "Sci-Fi"
+        let testMovie = Movie(title: title)
+        sut.addMovie(movie: testMovie)
+        XCTAssertEqual(sut.moviesToSee, 1)
+    }
+    
+    
+    func testQuery_ReturnsMovieAtIndex(){
+        let title = "Drama"
+        let testMovie = Movie(title: title)
+        sut.addMovie(movie: testMovie)
+        let movieQueried = sut.movieAtIndex(index: 0)
+        XCTAssertEqual(testMovie.title, movieQueried.title)
+        
+    }
+    
+    
+    
+    
+    
 }
