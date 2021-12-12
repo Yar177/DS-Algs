9052629fe0c7350fd9f7a5bbd486ed6e23f1e884
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-12 15:14:25 -0500

    multiPlatform init

---
+++ MultiPlatformApp/Shared/MultiPlatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlatformAppApp.swift
+//  Shared
+//
+//  Created by Hoshiar Sher on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
