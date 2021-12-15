9c92e2d045b5d07daa3ea4e1720c8e803c441f07
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-15 10:33:13 -0500

    cls

--- MultiPlatformApp/Shared/DataModels/DataRequest.swift
+++ MultiPlatformApp/Shared/DataModels/DataRequest.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import CoreLocation
+
+enum GetDataRequest<Type>{
+    case success([Type])
+    case failuer
+}
+
+
+
+struct DataRequest<Type> where Type: Decodable{
+    let basePath = "https://explorecalifornia.org/api/weather/city/"
+    let dataURL:URL
+    
+    init(city: String){
+        let dataPathString = (basePath + city).addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlHostAllowed)!
+        guard let dataUrl = URL(string: dataPathString) else { fatalError()}
+        self.dataURL = dataUrl
+    }
+    
+    func getData(completion: @escaping (GetDataRequest<Type>) -> Void){
+        let dataTask = URLSession.shared.dataTask(with: dataURL) { data, response, error in
+//            guard
+        }
+    }
+}
