2acd70875ce1229557399f95bb2b2e6593339a52
Hoshiar Sher <hoshiar.sher@excellus.com>
2021-12-19 16:49:37 -0500

    MyWeatherView

--- MultiPlatformApp/Shared/MyWeatherView.swift
+++ MultiPlatformApp/Shared/MyWeatherView.swift
@@ -6,8 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
 
 class MyWeatherView: View {
+    @State private var forecast =  [Forecast]()
+    @State private var selection: Forecast?
     
+    var body: some View{
+        List(selection: $selection){
+            ForEach(forecast) { forecast in
+                NavigationLink(destination: WeatherDetail(forecast: forecast), tag: forecast, selection: $selection){
+                    WeatherRow(forecast: forecast)
+                }
+            }
+        }.onAppear(perform: loadData)
+            .background(Rectangle().fill(BackgroundStyle()).edgesIgnoringSafeArea(.all))
+            .navigationBarTitle("🌤 Forecast")
+    }
+    
+    func loadData(){
+        let
+    }
 
 }
