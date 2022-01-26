0f67a810bef1986939ea5e78884a93d9dd0859ba
Yar Sher <yarsher.dev@gmail.com>
2022-01-25 23:51:01 -0500

    KeyChainFacade

--- SwiftGeneral.playground/Pages/SwiftSecurity.xcplaygroundpage/Contents.swift
+++ SwiftGeneral.playground/Pages/SwiftSecurity.xcplaygroundpage/Contents.swift
@@ -7,10 +7,17 @@ import Security
 //Asymeteric crypto
 enum KeychainError: Error{
     case keyGenarationError
+    case failure(status: OSStatus)
 }
 
 class KeyChainFacade{
-    
+    lazy var privateKey: SecKey? = {
+        guard let key = try? retrivePrivateKey(),
+              key != nil else{
+                  return try? generatePrivateKey()
+              }
+        return key
+    } as! SecKey
     private static let tagData = "com.asymriptodemo.keys.mkey".data(using: String.Encoding.utf8)!
     private let keyAttribute:[String : Any] = [kSecAttrType as String : kSecAttrKeyTypeRSA,
                                              kSecAttrKeySizeInBits as String: 2048,
@@ -27,7 +34,18 @@ class KeyChainFacade{
     private func retrivePrivateKey() throws -> SecKey? {
         let privateKeyQuery: [String: Any] = [ kSecClass as String : kSecClassKey,
                                                kSecAttrApplicationTag as String : KeyChainFacade.tagData,
-                                                      kSecAttrType as String : kSecAttrKeyTypeRSA]
+                                                      kSecAttrType as String : kSecAttrKeyTypeRSA,
+                                               kSecReturnRef as String: true]
+        var privateKeyRef: CFTypeRef?
+        let status = SecItemCopyMatching(privateKeyQuery as CFDictionary, &privateKeyRef)
+        guard status == errSecSuccess else {
+            if status == errSecItemNotFound {
+                return nil
+            } else {
+                throw KeychainError.failure(status: status)
+            }
+        }
+        return privateKeyRef != nil ? (privateKeyRef as! SecKey) : nil
     }
     
 }
