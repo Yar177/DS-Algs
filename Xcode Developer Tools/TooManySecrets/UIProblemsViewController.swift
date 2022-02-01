67569e33f41240cd5fcd9bfd0491ef7dadc37198
Yar Sher <yarsher.dev@gmail.com>
2022-02-01 10:53:01 -0500

    ui debug

--- Xcode Developer Tools/TooManySecrets/UIProblemsViewController.swift
+++ Xcode Developer Tools/TooManySecrets/UIProblemsViewController.swift
@@ -12,13 +12,14 @@ class UIProblemsViewController: UIViewController {
     
     private func addImage() {
         let briefcaseImage = UIImageView(image: #imageLiteral(resourceName: "briefcase"))
-        
+        briefcaseImage.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(briefcaseImage)
         
         NSLayoutConstraint.activate([
             briefcaseImage.widthAnchor.constraint(equalToConstant: 300),
             briefcaseImage.heightAnchor.constraint(equalToConstant: 300),
             briefcaseImage.topAnchor.constraint(equalTo: topSecretLabel.bottomAnchor, constant: 10),
+            briefcaseImage.centerXAnchor.constraint(equalTo: topSecretLabel.centerXAnchor)
         ])
     }
 }
