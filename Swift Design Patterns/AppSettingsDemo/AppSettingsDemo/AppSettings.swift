34c1c0a73cd888c39fe471342b34f0ff4ceb697a
Yar Sher <yarsher.dev@gmail.com>
2022-02-03 20:24:11 -0500

    fix concurancy

---
+++ Swift Design Patterns/AppSettingsDemo/AppSettingsDemo/AppSettings.swift
@@ -0,0 +1,43 @@
+//
+//  AppSettings.swift
+//  AppSettingsDemo
+//
+//  Created by Karoly Nyisztor on 2/11/19.
+//  Copyright © 2019 Karoly Nyisztor. All rights reserved.
+//
+
+import Foundation
+
+final public class AppSettings {
+    
+    public static let shared = AppSettings()
+    
+    private let serialQueue = DispatchQueue(label: "serialQueue")
+    
+    private var settings: [String: Any] = ["Theme": "Dark",
+                                           "MaxConsurrentDownloads": 4]
+    
+    private init() {}
+    
+    public func string(forKey key: String) -> String? {
+        var result: String?
+        serialQueue.sync {
+            result =  settings[key] as? String
+        }
+        return result
+    }
+    
+    public func int(forKey key: String) -> Int? {
+        var result: Int?
+        serialQueue.sync {
+            result = settings[key] as? Int
+        }
+        return result
+    }
+    
+    public func setValueForKey(value: Any, forKey key: String){
+        serialQueue.sync {
+            settings[key] = value
+        }
+    }
+}
