3396a059d77e1c85c348d165bc240ca12799d271
Yar Sher <yarsher.dev@gmail.com>
2022-02-22 13:01:40 -0500

    state machine

--- StateMachineCoffeeMachine.playground/Pages/CoffeeMachine.xcplaygroundpage/Contents.swift
+++ StateMachineCoffeeMachine.playground/Pages/CoffeeMachine.xcplaygroundpage/Contents.swift
@@ -1,10 +1,84 @@
 import Foundation
 
-class CoffeeMachine {
-    private var isWaterTankFilled: Bool
-    private var isCapsuleBinEmpty: Bool
-    private var isCapsuleInserted: Bool
+fileprivate protocol CoffeeMachineState {
+    func isReadyToBrew() -> Bool
+    func brew()
+}
 
+extension CoffeeMachineState{
+    func isReadyToBrew() -> Bool{
+        print("\(#function) not implimented for \(self) state")
+        return false
+    }
+    
+    func brew(){
+        print("\(#function) not implimented for \(self) state")
+    }
+}
+
+
+fileprivate struct StandbyState: CoffeeMachineState{
+    
+}
+
+fileprivate struct FillWaterTankStatte: CoffeeMachineState{
+    var context: CoffeeMachine
+    
+    func isReadyToBrew() -> Bool {
+        guard context.isWaterTankFilled else{
+            print("Fill Water Tank")
+            context.state = StandbyState()
+            return false
+        }
+        context.state = EptyCapusleBinStatte(context: context)
+        return context.state.isReadyToBrew()
+    }
+    
+}
+
+struct EptyCapusleBinStatte: CoffeeMachineState{
+    var context: CoffeeMachine
+    
+    func isReadyToBrew() -> Bool {
+        guard context.isCapsuleBinEmpty else{
+            print("no capsule")
+            context.state = StandbyState()
+            return false
+        }
+        context.state = InsurtCapsuleStatte(context: context)
+        return context.state.isReadyToBrew()
+    }
+
+}
+
+struct InsurtCapsuleStatte: CoffeeMachineState{
+    var context: CoffeeMachine
+    
+    func isReadyToBrew() -> Bool {
+        guard context.isCapsuleInserted else{
+            print("insert coffee copsule")
+            context.state = StandbyState()
+            return false
+        }
+        return true
+    }
+
+}
+
+struct BrewCoffeeStatte: CoffeeMachineState{
+    var context: CoffeeMachine
+    
+
+}
+
+class CoffeeMachine {
+    fileprivate var isWaterTankFilled: Bool
+    fileprivate var isCapsuleBinEmpty: Bool
+    fileprivate var isCapsuleInserted: Bool
+    
+    fileprivate var state: CoffeeMachineState = StandbyState()
+    
+    
     required init(waterFilled: Bool, binEmpty: Bool, capsuleInserted: Bool) {
         isWaterTankFilled = waterFilled
         isCapsuleBinEmpty = binEmpty
