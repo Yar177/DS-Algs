cb8fdd0029fede67a112c8831f60b2ab44e8a525
Yar Sher <yarsher.dev@gmail.com>
2022-02-22 10:33:22 -0500

    subject

--- Broadcasting.playground/Contents.swift
+++ Broadcasting.playground/Contents.swift
@@ -1,6 +1,48 @@
 import UIKit
 import PlaygroundSupport
 
+
+protocol Observer {
+    func notify()
+    var uid: Int {get}
+}
+
+protocol Subject {
+    mutating func register(_ obeserver: Observer)
+    mutating func unregister(_ obeserver: Observer)
+    
+}
+
+extension UILabel: Observer{
+    func notify() {
+        self.text = "Subject state changed"
+    }
+    
+    var uid: Int {
+        return ObjectIdentifier(self).hashValue
+    }
+}
+
+
+extension UIButton: Subject{
+    private static var observers = [Observer]()
+    
+    func register(_ obeserver: Observer) {
+        UIButton.observers.append(obeserver)
+    }
+    
+    func unregister(_ obeserver: Observer) {
+        UIButton.observers = UIButton.observers.filter{$0.uid != obeserver.uid}
+    }
+    
+    func onStateChanged(){
+        UIButton.observers.forEach { (observer) in
+            observer.notify()
+        }
+    }
+    
+}
+
 class MyViewController : UIViewController {
     var labels = [UILabel]()
     
@@ -24,7 +66,7 @@ class MyViewController : UIViewController {
         
         for i in 0 ..< labelCount {
             let label = UILabel(frame: CGRect(x: 0, y: 200 + (i * 30), width: 380, height: 20))
-            label.text = "Label # \(i + 1) listening..."
+            label.text = "Label # \(i + 1) listening"
             label.textColor = .black
             label.textAlignment = .center
             view.addSubview(label)
