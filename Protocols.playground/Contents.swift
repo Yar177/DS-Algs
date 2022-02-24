93d5cdf08cf8918c726311772584efeb41a7f15a
Yar Sher <yarsher.dev@gmail.com>
2022-02-24 08:34:19 -0500

    protocol Taggable

---
+++ Protocols.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+protocol Taggable {
+    var tag : String{get set}
+    var data: Data{get}
+    static var counter: Int{get}
+    
+    mutating func update(data: Data) -> Bool
+    
+    static func incrementCounter()
+    
+    init(tag: String, data: Data)
+}
+
+
+
+
+
