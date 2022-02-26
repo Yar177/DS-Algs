eec6a3c4ca157f6af5864fd56a242a5e7c4c67d9
Yar Sher <yarsher.dev@gmail.com>
2022-02-25 22:27:43 -0500

    Protocol Composition

--- Protocols.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -34,4 +34,50 @@ struct TaggedDate: Taggable{
     }
 }
 
+protocol TaggableInheratance {
+    var tag : String{get set}
+    var data: Data{get}
+    init(tag: String, data: Data)
+}
+
+protocol TaggedPresistable: TaggableInheratance, CustomStringConvertible, Equatable{
+    init(tag: String, contentOf url: URL) throws
+    func presist(to url: URL) throws
+}
+
+
+protocol TaggedEncodable: TaggableInheratance{
+    var base64: String {get}
+}
+
+
+//Protocol Composition
+struct MyData: TaggedEncodable, TaggedPresistable{
+    var base64: String {
+        return self.data.base64EncodedString()
+    }
+    
+    init(tag: String, contentOf url: URL) throws {
+        let data = try Data.init(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+    
+    func presist(to url: URL) throws {
+        try self.data.write(to: url)
+    }
+    
+    var tag: String
+    
+    var data: Data
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+    
+    var description: String{
+        return "MyData(\(tag)"
+    }
+}
+
 
